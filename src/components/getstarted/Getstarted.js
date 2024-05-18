62552febc3df7bec385abde5a3494a5050192cf1
Sreethu K Binu <101237839+sreethukb@users.noreply.github.com>
2024-05-18 14:55:19 +0530

    Update Getstarted.js

--- src/components/getstarted/Getstarted.js
+++ src/components/getstarted/Getstarted.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import './getstarted.css'
-import product from '../../assets/—Pngtree—3d shopping basket and trolley_5896095.png'
-import service from '../../assets/Lovepik_com-380101363-customer-service-and-customer-call-service-scenario-work-phone-repair.png'
-
+import products from '../../assets/—Pngtree—3d shopping basket and trolley_5896095.png'
+import services from '../../assets/Lovepik_com-380101363-customer-service-and-customer-call-service-scenario-work-phone-repair.png'
+import { Link } from 'react-router-dom';
 
 const Getstarted = () => {
-
   return (
     <div className='getstarted-container'>
         <h2 id="choose">Choose what you want to <span id="typewriter">review.</span></h2>
         <div className='get-started'>         
           <div className='card'>
-          <img src={product} alt='shopping trolley' />
+            <Link to='product'>
+            <img src={products} alt='shopping trolley' />
             <p>Review Products</p>
+            </Link>
           </div>
           <div className='card'>
-            <img src={service} alt='customer call service' />
+            <Link to='service'>
+            <img src={services} alt='customer call service' />
             <p>Review Services</p>
+            </Link>
           </div>   
         </div>
+        
     </div>
   );
 }

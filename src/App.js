12bac7f5fe3b579708c622194dd2f66a4dff2779
Sreethu K Binu <101237839+sreethukb@users.noreply.github.com>
2024-05-18 14:54:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,35 @@ import Getstarted from './components/getstarted/Getstarted';
 import AdminLogin from './components/adminlogin/AdminLogin';
 import Contactus from './components/contact/Contactus';
 import Footer from './components/footer/Footer';
-
+import Product from './components/feedback/Product';
+import Service from './components/feedback/Service';
+import dashboard from './components/adminlogin/dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
     return (
-      <div>
-        <Navbar />
+      <Router>
+        <div className='navbar-class'>
+          <Navbar />
+        </div>
+        <div className='hero-class'>
         <Hero />
-        <Getstarted />
-        <AdminLogin />
-        <Contactus />
-        <Footer />
-      </div> 
+        </div>
+        <Routes>
+          <Route path='' element={<Getstarted/>}/>
+            <Route path='/product' element={<Product />}/>
+            <Route path='/service' element={<Service />}/>
+          <Route/>
+        </Routes>
+        <div>
+          <Route path="/login" component={<AdminLogin/>} />
+          <Route path="/dashboard" component={<dashboard/>} />
+          <Contactus />
+        </div>
+        <div className='footer-class'>
+          <Footer />
+        </div>
+      </Router>
   );
 };
 
